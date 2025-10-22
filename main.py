e08e2ebd783abe2976d65f5c48cd5728171aa9c1
REDsage8390 <bhambererushikesh4@gmail.com>
2025-10-22 15:52:58 +0530

    Modified commit-2

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def read_root():
     return {"message": "Hello,FastAPI!"}
 
 products = [
-    Product(id=1, name="phone",description="A smartphone",price=699.99,float=50),
-    Product(id=2, name="laptop",description="A machine",price=60999.99,float=50),
-    Product(id=3, name="tablet",description="A small computer",price=399.99,float=50),
+    Product(id=1, name="phone",description="A smartphone",price=699.99,quantity=50),
+    Product(id=2, name="laptop",description="A machine",price=60999.99,quantity=50),
+    Product(id=3, name="tablet",description="A small computer",price=399.99,quantity=50),
 ]
 
 @app.get("/products")
